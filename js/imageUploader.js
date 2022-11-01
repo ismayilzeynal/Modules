29442efe942c3c4eb8fd08036d95f70a9d736cde
ismayilzeynal <ismayilbz@code.edu.az>
2022-11-01 18:46:04 +0400

    problemli

---
+++ js/imageUploader.js
@@ -0,0 +1,46 @@
+// const imagesParentDiv = document.querySelector(".container .all-images");
+// export function imageUploader(files) {
+//   for (let file of files) {
+//     const fileReader = new FileReader();
+
+//     fileReader.onloadend = (e) => {
+//       const { result } = e.target;
+    
+//       const aTag=document.createElement("a");
+//       aTag.setAttribute("href","#");
+//       imagesParentDiv.append(aTag);
+//       aTag.innerHTML=`<img src="${result}">`;
+      
+//     };
+
+//     fileReader.readAsDataURL(file);
+//   }
+// }
+
+
+
+const images = document.querySelector(".all-images");
+
+export function imageUploader(files) {
+  const imageList = [];
+  for (let file of files) {
+    const fileReader = new FileReader();
+
+    fileReader.onloadend = (e) => {
+      const { result } = e.target;
+    
+      const img = document.createElement("img");
+      img.setAttribute("src", result);
+      const aTag=document.createElement("a");
+      aTag.setAttribute("href",result);
+
+      imageList.push({ fileName: file.name, result });
+
+      localStorage.setItem("imageSlides", JSON.stringify(imageList));
+      aTag.appendChild(img);
+      images.append(aTag);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,87 @@
+import { imageUploader } from "./imageUploader.js";
+const uploadIcon = document.querySelector(".icon i");
+const inputFile = document.querySelector("input");
+
+uploadIcon.addEventListener("click", function () {
+  this.nextElementSibling.click();
+});
+
+inputFile.addEventListener("change", function (e) {
+  const { files } = e.target;
+
+  imageUploader(files);
+});
+
+
+
+
+
+
+
+/* slider and popup*/
+let changableAttr;
+
+
+document.querySelectorAll(".all-images a").forEach(image => {
+  console.log("1");
+    image.addEventListener("click",function(e){
+        e.preventDefault();
+        changableAttr=this.children[0].getAttribute("src");
+        changeImgSrcAttr(changableAttr);
+        document.querySelector(".pop-container").style.display="flex";
+    })
+})
+
+document.querySelector(".fa-arrow-right").addEventListener("click",function(){
+    nextSlide();
+})
+
+document.querySelector(".fa-arrow-left").addEventListener("click",function(){
+    prevSlide();
+})
+
+function nextSlide()
+{
+    if(document.querySelector(`[src="${changableAttr}"]`).parentElement.nextElementSibling)
+        changableAttr=document.querySelector(`[src="${changableAttr}"]`).parentElement.nextElementSibling.children[0].getAttribute("src");                 
+    else
+        changableAttr=document.querySelector(".all-images").children[0].children[0].getAttribute("src");
+    changeImgSrcAttr(changableAttr);
+}
+
+function prevSlide()
+{
+    if(document.querySelector(`[src="${changableAttr}"]`).parentElement.previousElementSibling)
+        changableAttr=document.querySelector(`[src="${changableAttr}"]`).parentElement.previousElementSibling.children[0].getAttribute("src");                 
+    else
+        changableAttr=document.querySelector(".all-images").lastElementChild.children[0].getAttribute("src");
+    changeImgSrcAttr(changableAttr);
+}
+
+function changeImgSrcAttr(src)
+{
+  document.querySelector(".pop-container .popup .biggest").setAttribute("src",src);
+}
+
+// under this line all for closing popup 
+document.addEventListener("click", function(e){
+
+    if(e.target.classList.contains("pop-container"))
+        closePopup();
+})
+
+addEventListener('keydown', (e) => {
+    if (e.key === "Escape")
+        closePopup();
+    else if (e.key === "ArrowRight")
+        nextSlide();
+    else if(e.key === "ArrowLeft")
+        prevSlide();
+});
+
+document.querySelector(".fa-xmark").addEventListener("click",closePopup);
+
+function closePopup()
+{
+  document.querySelector(".pop-container").style.display="none";
+}
